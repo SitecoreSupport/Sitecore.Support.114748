592d7d9f340fbb8bad133fac24907a8a72c88436
Dmitry Kolinchuk <dmko@sitecore.net>
2017-10-17 13:55:00 +0300

    Makes the ReadQueryString setting to work on MVC Form

---
+++ src/Sitecore.Support.114748/FormDataManager.cs
@@ -0,0 +1,48 @@
+﻿using Sitecore.Forms.Mvc.Events;
+using Sitecore.Forms.Mvc.Interfaces;
+using Sitecore.Forms.Mvc.Models;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Forms.Mvc.Data
+{
+    public class FormDataManager : Sitecore.Forms.Mvc.Data.FormDataManager
+    {
+        public FormDataManager(IProcessorFactory processorFactory) : base(processorFactory)
+        {
+        }
+
+        protected override void FormLoadState(FormEventArgs args)
+        {
+            base.FormLoadState(args);
+
+            if (args.RequestType == RequestType.POST)
+            {
+                return;
+            }
+            if (HttpContext.Current == null)
+            {
+                return;
+            }
+                
+            if (HttpContext.Current.Request == null)
+            {
+                return;
+            }
+                
+            var request = HttpContext.Current.Request;
+
+            foreach (FieldModel fieldModel in args.Form.Sections.SelectMany(x => x.Fields))
+            {
+                var value = request.QueryString[fieldModel.Name];
+
+                if (string.IsNullOrEmpty(value))
+                {
+                    continue;
+                }
+                    
+                fieldModel.Value = value;
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.114748/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.114748")]
+[assembly: AssemblyProduct("Sitecore.Support.114748")]
+[assembly: ComVisible(false)]
